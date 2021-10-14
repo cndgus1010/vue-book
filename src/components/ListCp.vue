7703df6f019a3e3b36641a7675185eaaebce997e
cndggus1010 <chung1838@namver.com>
2021-10-14 21:56:42 +0900

    first commit

---
+++ src/components/ListCp.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>List</h1>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  name: "ListCp",
+  computed: {
+    ...mapGetters(["GET_BOOKS"]),
+  },
+  watch: {
+    GET_BOOKS: function (v) {
+      console.log(v);
+    },
+  },
+  created() {
+    console.log(this);
+    this.$store.dispatch("ACT_BOOKS");
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
